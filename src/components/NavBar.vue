1798c406ad385297f51e6feb9e443436ca2079a2
RuanFelipe <ruansilva6721@gmail.com>
2022-11-10 10:42:55 -0300

    configurando rotas iniciais

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <nav>
+            <router-link to="/">
+                <img src="" alt="">
+            </router-link>
+
+            <router-link to="/">Home</router-link> |
+            <router-link to="/pedidos">Pedidos</router-link>
+          </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavBar"
+
+}
+
+</script>
